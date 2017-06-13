bf7b7c5e3bb9a791798107b4fb4880c53dac6ee7
Chandrakanth <chandrakanth.g@pervacio.com>
2017-06-13 22:51:20 +0530

    Added IService

---
+++ batteryalertlib/src/main/java/com/gck/servicelib/IService.java
@@ -0,0 +1,13 @@
+package com.gck.servicelib;
+
+/**
+ * Created by Pervacio on 13-06-2017.
+ */
+
+public interface IService {
+    void startService();
+
+    void startServiceForSnooze();
+
+    void startServiceForDismiss();
+}
---
+++ batteryalertlib/src/main/java/com/gck/servicelib/ServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gck.servicelib;
+
+import com.gck.batteryalertlib.App;
+import com.gck.batteryalertlib.BatteryReceiverService;
+
+/**
+ * Created by Pervacio on 13-06-2017.
+ */
+
+public class ServiceImpl implements IService {
+    @Override
+    public void startService() {
+        BatteryReceiverService.startService(App.getInstance(), BatteryReceiverService.START_NORMAL);
+    }
+
+    @Override
+    public void startServiceForSnooze() {
+        BatteryReceiverService.startService(App.getInstance(), BatteryReceiverService.START_FOR_SNOOZE);
+    }
+
+    @Override
+    public void startServiceForDismiss() {
+        BatteryReceiverService.startService(App.getInstance(), BatteryReceiverService.START_FOR_DISMISS);
+    }
+}
